0d2750dbd8d09875c2ba5287b263acc871a12a2d
eva montgomery <eva.m.montgomery@gmail.com>
2019-12-10 08:50:41 -0500

    adds skeleton for exercise

---
+++ scripts/index.js
@@ -0,0 +1,44 @@
+const nameContainer = document.querySelector(".js-menu");
+
+//Extract names of categories
+// Write a function (named getCategories()) that returns an array of
+// the category names from the menu variable.
+// (Hint: use the Object.keys() function!)
+
+function getCategories(dict) {
+    // console.log(Object.keys(menu)); 
+    return (Object.keys(dict)); 
+}
+   let mealCategories = getCategories(menu);
+   console.log(mealCategories);
+
+// Wrap names in <li> elements
+// Write a function (named nameToListItem()) that accepts a category 
+// name (like "breakfast") and returns a new <li> DOM element with 
+// the category name set as its .textContent
+// Make sure to return the new element.
+
+function nameToListItem(categoryName) {
+    const li = document.createElement('li');
+    // li.appendChild(categoryName);
+    li.textContent = categoryName;
+    return li;
+}
+
+console.log(nameToListItem("breakfast"));
+
+// Write a function that transforms an array of category names to an 
+// array of <li> elements.
+
+
+
+function categoriesToListItem(array) {
+    return array.map(nameToListItem);
+    // for (let elementItem of categoryName) {
+    // li.appendChild(elementItem);
+}
+console.log(categoriesToListItem(mealCategories));
+
+// Use `getCategories()`in combination with `categoriesToListItem()`
+
+getCategories(categoriesToListItem());
